c43e192948e75a969b781206f8831be3b41a8176
Michał <michal.chimczak@gmail.com>
2019-09-15 15:03:23 +0200

    Update photos,styles and functions

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
 const navHeader = document.querySelector('.nav__header');
 const main = document.querySelector('.main__img');
-const headerContent = document.querySelector('.main__header__content')
+const headerContent = document.querySelector('.main__header__content');
 
 window.onload = function () {
-    // setTimeout(() => main.classList.add('enlarge'), 100)
-    // setTimeout(() => navHeader.classList.add('active-enter'), 450);
+
     setTimeout(() => navHeader.classList.add('active-enter'), 450);
 
-    var promise =  new Promise(function(resolve, reject) {
-        setTimeout(function() {
-            resolve(main.classList.add('enlarge'));
-         }, 100);
+    // var promise1 =  new Promise(function(resolve, reject) {
+    //     setTimeout(function() {
+    //         main.classList.add('enlarge')
+    //      }, 100);
+    //      setTimeout(function() {
+    //         resolve (headerContent.classList.add('showing'));
+    //     }, 1500);
+    // });
+    var promise1 = new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            main.classList.add('enlarge')
+        }, 100);
+        setTimeout(function () {
+            resolve(headerContent.classList.add('showing'));
+        }, 1500);
     });
-
-    // promise.then(setTimeout(() => headerContent.classList.add('showing')), 10000);
-    promise.then(setTimeout(function() {
-        headerContent.classList.add('showing');
-    }), 5000);
-
+    promise1.then(function () {
+        setTimeout(() => {
+            main.style.setProperty('filter', 'none');
+        }, 500);
+    });
 }
 
 
 
 
 // NAV MENU
+const page = document.querySelector('main');
 const menuIcon = document.querySelector('.nav__icon');
 const dropdownMenu = document.querySelector('.nav__dropdown-menu');
 const menu = document.querySelector('.nav__dropdown-menu__block');
@@ -32,11 +42,15 @@ const cross = document.querySelector('.nav__cross');
 const links = document.querySelectorAll('.link');
 
 function dropdown_toggle() {
+    page.classList.toggle('blur');
     menuIcon.classList.toggle('disable');
     cross.classList.toggle('active');
     dropdownMenu.classList.toggle('active');
     dropdownMenu.classList.toggle('active-enter')
     setTimeout(() => arrow.style.setProperty('opacity', '1'), 10);
+
+    page.classList.contains('blur') ? page.addEventListener('click', dropdown_toggle) : page.removeEventListener('click', dropdown_toggle);
+
 }
 
 menuIcon.addEventListener('click', dropdown_toggle);
@@ -49,7 +63,7 @@ links.forEach(link => link.addEventListener('click', dropdown_toggle));
 
 
 //  BUTTONS HANDLER 
-const buttons = document.querySelectorAll('.section__btn');
+const buttons = document.querySelectorAll('.section__btn:not(#s3btn)');
 
 buttons.forEach(button => button.addEventListener('click', () => {
     const nav = document.querySelector('nav');
@@ -68,6 +82,30 @@ buttons.forEach(button => button.addEventListener('click', () => {
     button.innerHTML === "Więcej" ? button.innerHTML = "Zwiń" : button.innerHTML = "Więcej";
     button.previousElementSibling.classList.toggle('section__content--hidden');
 }));
+
+
+// S3BTN EXCEPT !!!!!!!!!!1
+
+const s3btn = document.querySelector('#s3btn');
+s3btn.addEventListener('click', () => {
+    const nav = document.querySelector('nav');
+    let sectionContent = s3btn.parentNode;
+    let section = s3btn.parentNode.parentNode;
+
+
+    if (s3btn.innerHTML === "Więcej" || sectionContent.classList.contains("section__card")) {
+        window.scrollTo(0, sectionContent.offsetTop - nav.offsetHeight)
+    } else {
+        window.scrollTo(0, section.offsetTop - nav.offsetHeight)
+        // window.scrollTo(0, s3btn.offsetParent.offsetTop - nav.offsetHeight)
+    }
+
+
+    s3btn.innerHTML === "Więcej" ? s3btn.innerHTML = "Zwiń" : s3btn.innerHTML = "Więcej";
+    s3btn.previousElementSibling.classList.toggle('flex');
+});
+
+
 
 
 // SECTION 5 GALLERY SLIDESHOW
@@ -103,7 +141,7 @@ function highlightElement() {
         const text = this.children[2];
         const info = this.children[3];
         // text.style.setProperty('display', 'block') && info.style.setProperty('display', 'block');
-        photo.style.setProperty('transform', 'translate(-50%) scale(1.4)');
+        window.innerWidth >= 992 ? photo.style.setProperty('transform', 'translate(-50%) scale(1.4)') : '';
         header.classList.add('highlight');
         text.classList.add('active') || info.classList.add('active');
         setTimeout(() => text.classList.add('active-enter') || info.classList.add('active-enter'),150);
@@ -132,7 +170,7 @@ function highlightElement() {
         const header = this.children[1];
         const text = this.children[2];
         // text.style.setProperty('display', 'block');
-        photo.style.setProperty('transform', 'translate(-50%) scale(1.4)');
+        window.innerWidth >= 992 ? photo.style.setProperty('transform', 'translate(-50%) scale(1.4)'): '';
         header.classList.add('highlight');
         text.classList.add('active');
         setTimeout(() => text.classList.add('active-enter'),150);
@@ -158,7 +196,7 @@ function blindElement() {
     const text = this.children[2];
     // text.style.setProperty('display', 'none');
     // highlight.style.setProperty('display', 'none');
-    photo.style.setProperty('transform', 'translate(-50%) scale(1)');
+    window.innerWidth >= 992 ? photo.style.setProperty('transform', 'translate(-50%) scale(1)'): '';
     header.classList.remove('highlight');
     text.classList.remove('active', 'active-enter') || highlight.classList.remove('active-enter');
     
@@ -210,3 +248,30 @@ function debounce(func, wait = 20, immediate = true) {
   }
 
   window.addEventListener('scroll', debounce(checkSlide));
+
+
+
+
+
+
+
+
+
+
+
+  function changeHeaderContent() {
+      const headerContent = document.querySelector('.main__header__content > span');
+      const header = document.querySelector('.main__header__content');
+    
+
+      if (window.innerWidth >= 992) {
+          headerContent.innerHTML = "<p>Specjalistyczna Praktyka Stomatologiczna OrtoDentis zajmująca się leczeniem <span class='highlight' style='font-family:Istok Web', sans-serif'>ortodontycznym</span> funkcjonuje od 1994 roku.</p> <p>Do Państwa dyspozycji są nowocześnie wyposażone gabinety stomatologiczne oraz <span class='highlight' style='font-family:Istok Web', sans-serif'>pracownia RTG</span>.</p> <p>Pracujemy na najnowocześniejszych i sprawdzonych materiałach renomowanych firm ortodontycznych.</p><br><p>Zapoznaj się z naszą ofertą!</p>"
+        } else {
+        headerContent.innerHTML = "<p>Nowoczesne metody leczenia, profesjonalna opieka ortodontyczna i doświadczenie zbierane od ponad <span class='highlight' style='font-family:Istok Web', sans-serif'>25 lat</span>.</p><p>Zapoznaj się z naszą ofertą!</p>";
+      }
+
+  }
+
+  changeHeaderContent();
+
+  window.addEventListener('resize', debounce(changeHeaderContent));
